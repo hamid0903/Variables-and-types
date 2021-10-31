7902d75fbdff17da2316f52b6df87f674cddf007
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-10-31 18:35:23 +0500

    Num05

---
+++ begin05.py
@@ -0,0 +1,9 @@
+# a o'zgaruvchiga 7 ni taminlang;
+# b o'zgaruvchiga 3 ni taminlang;
+# c o'zgaruvchiga 5 ni taminlang;
+# a, b, c o'zgaruvchilar yig'indisini chop eting;
+
+a =7
+b =3
+c =5
+print(a+b+c)
