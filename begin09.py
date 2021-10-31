071b765b4ab87aa5d7671476053ffab7f9099336
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-10-31 18:40:41 +0500

    Num09

---
+++ begin09.py
@@ -0,0 +1,13 @@
+# a o'zgaruvchiga 8 ni taminlang;
+# b o'zgaruvchiga 3 ni taminlang;
+# c o'zgaruvchiga 2 ni taminlang;
+# d o'zgaruvchiga 4 ni taminlang;
+# K =  a + b - c * d;
+# Natijani chop eting;
+
+a = 8
+b = 3
+c = 2
+d = 4
+K=a + b - c * d
+print(K)
