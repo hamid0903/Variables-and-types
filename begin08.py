95c20699d51f4f5d4be62b4c37459ef90cf291dd
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-10-31 18:39:59 +0500

    Num08

---
+++ begin08.py
@@ -0,0 +1,12 @@
+# a o'zgaruvchiga 5 ni taminlang;
+# b o'zgaruvchiga 4 ni taminlang;
+# c o'zgaruvchiga 8 ni taminlang;
+# Quyidagi ifodani K ga taminglang;
+# K =  (a * b) / c
+# Natijani chop eting;
+
+a = 5
+b = 4
+c = 8
+K=(a * b) / c
+print(K)
