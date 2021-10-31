7916ebf3c9173965f2a15eff76ee28a56ebac6a4
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-10-31 18:37:56 +0500

    Num07

---
+++ begin07.py
@@ -0,0 +1,11 @@
+# a o'zgaruvchiga 5 ni taminlang;
+# b o'zgaruvchiga 4 ni taminlang;
+# c o'zgaruvchiga 8 ni taminlang;
+# Quyidagi ifodani natijaga chop eting;
+# ifoda: (2*a/b)*c
+
+a = 5
+b = 4
+c = 8
+x=(2*a/b)*c
+print(x)
