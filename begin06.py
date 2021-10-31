87fc8b3948382658c6474a6abb815dad53c5c743
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-10-31 18:36:56 +0500

    Num06

---
+++ begin06.py
@@ -0,0 +1,12 @@
+# a o'zgaruvchiga 24 ni taminlang;
+# b o'zgaruvchiga 3 ni taminlang;
+# c o'zgaruvchiga 5 ni taminlang;
+# Quyidagi ifodani K ga taminglang;
+# K =  a - b + c;
+# Natijani chop eting;
+
+a =24
+b =3
+c =5
+K=a-b+c
+print(K)
